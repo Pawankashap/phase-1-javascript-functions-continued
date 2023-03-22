2dc1a40ff67796cfffcb6faeba051e818ce12da6
Fnu Pawan Kumar <pawan.3940@gmail.com>
2023-03-22 00:40:42 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+ function saturdayFun (paraValue='roller-skate') {
+     return `This Saturday, I want to ${paraValue}!`
+ }
+
+ const mondayWork = function(paraValue='go to the office'){
+   return `This Monday, I will ${paraValue}.`;
+}
+
+function wrapAdjective(paraValue1='*') {
+    if (paraValue1==='*') {
+           const innerFunction= function(paraValue='special') {
+            return `You are ${paraValue1}a hard worker${paraValue1}!`
+            }
+            return innerFunction;
+    }
+    else if (paraValue1==='||'){
+        const innerFunction= function(paraValue='special') {
+            return `You are ${paraValue1}a dedicated programmer${paraValue1}!`
+            }
+            return innerFunction;
+    }
+}
+
+
